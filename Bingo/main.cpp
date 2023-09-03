76a15fb5067d88a03ae4cf45ab7e2c5c2cb89038
Brunito <thulobruno@gmail.com>
2023-09-02 23:34:43 -0500

    commit message

---
+++ Bingo/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_studio(false);
+    cin.tie(0);
+    int n; cin>>n;
+    for(int i=0; i<n;i++){
+        int x, y, z;
+        //Ingrese las coordenadas
+        scanf("%d %d %d",&x, &y, &z); 
+        sum=0;
+        sum+=x+y+z;
+    }
+    if(sum==o) cout<<"YES";
+    else if cout<<"NO";
+    else if
+    return 0;
+}
